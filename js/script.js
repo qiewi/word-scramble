cd0506fb048a66fbba0371a8d92e4665c81e9604
qiewi <105619444+qiewi@users.noreply.github.com>
2024-10-19 10:35:34 +0700

    randomizing words

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    console.log(wordArray, randomObj.word);
+}
+
+initGame();
